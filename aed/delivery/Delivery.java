096769e046c76ef80586992f4963b692b8612dbb
Manuel <manu.lopezc.corredor@gmail.com>
2020-12-11 14:32:16 +0100

    Tour acabado, falta merge con length

--- aed/delivery/Delivery.java
+++ aed/delivery/Delivery.java
@@ -2,8 +2,13 @@ package aed.delivery;
 
 import es.upm.aedlib.positionlist.NodePositionList;
 import es.upm.aedlib.positionlist.PositionList;
+import es.upm.aedlib.set.PositionListSet;
+
+import java.util.Iterator;
+
 import es.upm.aedlib.graph.DirectedAdjacencyListGraph;
 import es.upm.aedlib.graph.DirectedGraph;
+import es.upm.aedlib.graph.Edge;
 import es.upm.aedlib.graph.Vertex;
 import es.upm.aedlib.indexedlist.ArrayIndexedList;
 import es.upm.aedlib.indexedlist.IndexedList;
@@ -15,8 +20,7 @@ public class Delivery<V> {
 
   // Construct a graph out of a series of vertices and an adjacency matrix.
   // There are 'len' vertices. A negative number means no connection. A
-  // non-negative
-  // number represents distance between nodes.
+  // non-negative number represents distance between nodes.
   public Delivery(V[] places, Integer[][] gmat) {
     graph = new DirectedAdjacencyListGraph<>();
     verteces = new ArrayIndexedList<>();
@@ -26,7 +30,7 @@ public class Delivery<V> {
 
     for (int i = 0; i < gmat.length; i++) {
       for (int j = 0; j < gmat[i].length; j++) {
-        if (gmat[i][j] != null)
+        if (gmat[i][j] != null && gmat[i][j] >= 0)
           graph.insertDirectedEdge(verteces.get(i), verteces.get(j), gmat[i][j]);
       }
     }
@@ -42,7 +46,29 @@ public class Delivery<V> {
   // The list containts a series of vertices, with no repetitions (even if the
   // path can be expanded to a cycle).
   public PositionList<Vertex<V>> tour() {
-    return null;
+    Iterator<Vertex<V>> vertexIt = graph.vertices().iterator();
+    PositionList<Vertex<V>> result = null;
+    while (vertexIt.hasNext() && result == null) {
+      result = tour(vertexIt.next(), new PositionListSet<>());
+    }
+    return result;
+  }
+
+  private PositionList<Vertex<V>> tour(Vertex<V> activeV, PositionListSet<Vertex<V>> visited) {
+    visited.add(activeV);
+    PositionList<Vertex<V>> res = null;
+    for (Edge<Integer> edge : graph.outgoingEdges(activeV)) {
+      if (!visited.contains(graph.endVertex(edge)) && res == null)
+        res = tour(graph.endVertex(edge), visited);
+    }
+    if (visited.size() == verteces.size())
+      res = new NodePositionList<>();
+
+    if (res != null)
+      res.addFirst(activeV);
+    visited.remove(activeV);
+
+    return res;
   }
 
   public int length(PositionList<Vertex<V>> path) {
